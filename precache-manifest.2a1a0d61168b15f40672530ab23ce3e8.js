9f55235abf65fb8c9f876f2115f9c5d7b44b7337
sinsinkun <returntosender1020@hotmail.com>
2020-09-20 16:11:02 -0400

    Updates

---
+++ precache-manifest.2a1a0d61168b15f40672530ab23ce3e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "944602333802c907670d5cab4b24a7c5",
+    "url": "/react-website/index.html"
+  },
+  {
+    "revision": "b6ce0d59872595c64e36",
+    "url": "/react-website/static/css/main.07432542.chunk.css"
+  },
+  {
+    "revision": "ae4ab17e89689c94c7fa",
+    "url": "/react-website/static/js/2.2f43d5e3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-website/static/js/2.2f43d5e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6ce0d59872595c64e36",
+    "url": "/react-website/static/js/main.d7d2e487.chunk.js"
+  },
+  {
+    "revision": "fadfad2d28377605b9ac",
+    "url": "/react-website/static/js/runtime-main.caee497b.js"
+  },
+  {
+    "revision": "c97a9fc29652bb4afcdac68020e5d0f7",
+    "url": "/react-website/static/media/Merriweather-Regular.c97a9fc2.ttf"
+  },
+  {
+    "revision": "80abe26d7431faf0e1c48ff56a8bd428",
+    "url": "/react-website/static/media/space2.80abe26d.jpg"
+  }
+]);
