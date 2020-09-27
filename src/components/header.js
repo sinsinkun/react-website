51cc9a2ebf7c4d14db5b15ceb86839048350f5c7
sinsinkun <returntosender1020@hotmail.com>
2020-09-27 13:48:49 -0400

    Added animation to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,29 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/App.css';
 
 function Header(props) {
 
+	const [key, setKey] = useState(0);
+
+	useEffect(()=> {
+		//rerender component on props change
+		toggleKey();
+	}, [props.currentPage]) 
+
+	const toggleKey = () => {
+		if (key) setKey(0);
+		else setKey(1);
+	}
+
 	if (props.currentPage === 'home') {
 		return (
 			<div className = 'header' style={{background:'linear-gradient(#1abc9c, #dff)'}}>
-				<h1>Home Page</h1>
-				<p>Information {'&'} Filler Text</p>
+				<h1 id = {key}>Home Page</h1>
+				<p id = {key + 1}>Information {'&'} Filler Text</p>
 			</div>
 		);
 	}
 	else if (props.currentPage === 'demos') {
 		return (
 			<div className = 'header' style={{background:'linear-gradient(#dc6a9c, #dff)'}}>
-				<h1>Demos Page</h1>
-				<p>React application samples</p>
+				<h1 id = {key + 2}>Demos Page</h1>
+				<p id = {key + 3}> React application samples</p>
 			</div>
 		);
 	}
 	else if (props.currentPage === 'about') {
 		return (
 			<div className = 'header' style={{background:'linear-gradient(#15b2c4, #dff)'}}>
-				<h1>About Page</h1>
-				<p>Information on site {'&'} me</p>
+				<h1 id = {key + 4}>About Page</h1>
+				<p id = {key + 5}>Information on site {'&'} me</p>
 			</div>
 		);
 	}
