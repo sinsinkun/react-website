1dc6f1dd0314be41cf540278c13ba240da96eb3b
sinsinkun <returntosender1020@hotmail.com>
2020-10-18 09:18:15 -0400

    namecheap connection

--- src/components/Demos.js
+++ src/components/Demos.js
@@ -12,9 +12,18 @@ function Demos(props) {
 	return (
 		<div className = 'demos'>
 			<h3>Demos and stuff</h3>
+			{noDemo}
 			<p>More mini projects coming soon</p>
 		</div>
 	);
 }
+
+const noDemo = (
+	<div>
+		<p>This demo requires a larger screen size. 
+			Please make your browser window larger, 
+			or use a different device to view this demo.</p>
+	</div>
+)
 
 export default Demos;
