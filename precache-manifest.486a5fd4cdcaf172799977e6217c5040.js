8769d132d760dab7b54045610a6414bb34199c3d
sinsinkun <returntosender1020@hotmail.com>
2020-10-19 14:00:35 -0400

    Updates

---
+++ precache-manifest.486a5fd4cdcaf172799977e6217c5040.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1607c77a041f570639ca56c82e5199f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5f5c7d8ccaa050394ef",
+    "url": "/static/css/main.6aa72661.chunk.css"
+  },
+  {
+    "revision": "291a382203f019b502d6",
+    "url": "/static/js/2.41240ed8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.41240ed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f5c7d8ccaa050394ef",
+    "url": "/static/js/main.986a007d.chunk.js"
+  },
+  {
+    "revision": "f57bd6b3cd488554ebce",
+    "url": "/static/js/runtime-main.39870823.js"
+  },
+  {
+    "revision": "cd619a4f068dc66cc6e58fe0a91a8a34",
+    "url": "/static/media/Rubik-Regular.cd619a4f.ttf"
+  },
+  {
+    "revision": "440110ce5a97042d41e8a6b78e0d9b8a",
+    "url": "/static/media/personal_logo_inverted.440110ce.png"
+  },
+  {
+    "revision": "1a949599ae8238d9aebcc50caf2484d0",
+    "url": "/static/media/profilePhotoS.1a949599.jpg"
+  },
+  {
+    "revision": "21c3d42b5c25bef500bda33f0fe0ed5e",
+    "url": "/static/media/space1.21c3d42b.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6a834752caaa405d0708cfacdab4da0.js"
+  "/precache-manifest.486a5fd4cdcaf172799977e6217c5040.js"
 );
 
 self.addEventListener('message', (event) => {
