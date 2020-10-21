e2595b8a90dee6f6572f668afccfafb49ceaad58
sinsinkun <returntosender1020@hotmail.com>
2020-10-21 12:31:37 -0400

    styled demo 1 pieces

--- src/components/Demo1.js
+++ src/components/Demo1.js
@@ -4,13 +4,13 @@ import '../css/Demo1.css';
 
 function Demo1() {
 
-  const [piece1Pos, setPiece1Pos] = useState({x:30, y:250})
-  const [piece2Pos, setPiece2Pos] = useState({x:30, y:340})
-  const [piece3Pos, setPiece3Pos] = useState({x:110, y:200})
-  const [piece4Pos, setPiece4Pos] = useState({x:110, y:290})
-  const [piece5Pos, setPiece5Pos] = useState({x:110, y:380})
-  const [piece6Pos, setPiece6Pos] = useState({x:190, y:250})
-  const [piece7Pos, setPiece7Pos] = useState({x:190, y:340})
+  const [piece1Pos, setPiece1Pos] = useState({x:30, y:250, flipped:false})
+  const [piece2Pos, setPiece2Pos] = useState({x:30, y:340, flipped:false})
+  const [piece3Pos, setPiece3Pos] = useState({x:110, y:200, flipped:false})
+  const [piece4Pos, setPiece4Pos] = useState({x:110, y:290, flipped:false})
+  const [piece5Pos, setPiece5Pos] = useState({x:110, y:380, flipped:false})
+  const [piece6Pos, setPiece6Pos] = useState({x:190, y:250, flipped:false})
+  const [piece7Pos, setPiece7Pos] = useState({x:190, y:340, flipped:false})
 
   const onStop = (e, d) => {
     const setPieceList = [setPiece1Pos, setPiece2Pos, setPiece3Pos, 
@@ -25,15 +25,14 @@ function Demo1() {
           let foundPlacement = false;
           for (let j=0; j<7; j++) {
             if ( d.x > placementSpots[j].x - 30 && d.x < placementSpots[j].x + 30) {
-              setPieceList[i]({x: placementSpots[j].x, y: 90})
+              setPieceList[i]({x: placementSpots[j].x, y: 90, flipped:true})
               foundPlacement = true;
             }
           }
-          if (!foundPlacement) setPieceList[i]({x: d.x, y: d.y})
+          if (!foundPlacement) setPieceList[i]({x: d.x, y: d.y, flipped:false})
         }
-        else setPieceList[i]({x: d.x, y: d.y})
+        else setPieceList[i]({x: d.x, y: d.y, flipped:false})
       }
-      else console.log('drag error');
     }
   }
 
@@ -51,44 +50,44 @@ function Demo1() {
       </div>
       <Draggable position={piece1Pos} positionOffset={{x:0,y:-180}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p0' className='hex' style={{backgroundColor:'rgb(20,200,200)'}}>
-          {`x: ${piece1Pos.x} y: ${piece1Pos.y}`}
+        <div id='p0' className={piece1Pos.flipped ? 'hex flipped' : 'hex'}>
+          {piece1Pos.flipped ? '' : '\u265A'}
         </div>
       </Draggable>
       <Draggable position={piece2Pos} positionOffset={{x:0,y:-260}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p1' className='hex' style={{backgroundColor:'rgb(200,20,200)'}}>
-          {`x: ${piece2Pos.x} y: ${piece2Pos.y}`}
+        <div id='p1' className={piece2Pos.flipped ? 'hex flipped' : 'hex'}>
+          {piece2Pos.flipped ? '' : '\u265C'}
         </div>
       </Draggable>
       <Draggable position={piece3Pos} positionOffset={{x:0,y:-340}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p2' className='hex' style={{backgroundColor:'rgb(200,200,20)'}}>
-          {`x: ${piece3Pos.x} y: ${piece3Pos.y}`}
+        <div id='p2' className={piece3Pos.flipped ? 'hex flipped' : 'hex'}>
+          {piece3Pos.flipped ? '' : '\u265E'}
         </div>
       </Draggable>
       <Draggable position={piece4Pos} positionOffset={{x:0,y:-420}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p3' className='hex' style={{backgroundColor:'rgb(20,100,100)'}}>
-          {`x: ${piece4Pos.x} y: ${piece4Pos.y}`}
+        <div id='p3' className={piece4Pos.flipped ? 'hex flipped' : 'hex'}>
+          {piece4Pos.flipped ? '' : '\u2660'}
         </div>
       </Draggable>
       <Draggable position={piece5Pos} positionOffset={{x:0,y:-500}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p4' className='hex' style={{backgroundColor:'rgb(100,20,100)'}}>
-          {`x: ${piece5Pos.x} y: ${piece5Pos.y}`}
+        <div id='p4' className={piece5Pos.flipped ? 'hex flipped' : 'hex'}>
+          {piece5Pos.flipped ? '' : '\u2663'}
         </div>
       </Draggable>
       <Draggable position={piece6Pos} positionOffset={{x:0,y:-580}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p5' className='hex' style={{backgroundColor:'rgb(100,100,20)'}}>
-          {`x: ${piece6Pos.x} y: ${piece6Pos.y}`}
+        <div id='p5' className={piece6Pos.flipped ? 'hex flipped' : 'hex'}>
+          {piece6Pos.flipped ? '' : '\u2665'}
         </div>
       </Draggable>
       <Draggable position={piece7Pos} positionOffset={{x:0,y:-660}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p6' className='hex' style={{backgroundColor:'rgb(100,200,100)'}}>
-          {`x: ${piece7Pos.x} y: ${piece7Pos.y}`}
+        <div id='p6' className={piece7Pos.flipped ? 'hex flipped' : 'hex'}>
+          {piece7Pos.flipped ? '' : '\u2666'}
         </div>
       </Draggable>
 	  </div>
