60b78faf93cf137c48d26784907980c42833f781
sinsinkun <returntosender1020@hotmail.com>
2020-10-20 21:56:41 -0400

    made tiles snap to placement spots

--- src/components/Demo1.js
+++ src/components/Demo1.js
@@ -13,16 +13,28 @@ function Demo1() {
   const [piece7Pos, setPiece7Pos] = useState({x:190, y:340})
 
   const onStop = (e, d) => {
-    if (e.target.id === 'p1') setPiece1Pos({x: d.x, y: d.y});
-    else if (e.target.id === 'p2') setPiece2Pos({x: d.x, y: d.y});
-    else if (e.target.id === 'p3') setPiece3Pos({x: d.x, y: d.y});
-    else if (e.target.id === 'p4') setPiece4Pos({x: d.x, y: d.y});
-    else if (e.target.id === 'p5') setPiece5Pos({x: d.x, y: d.y});
-    else if (e.target.id === 'p6') setPiece6Pos({x: d.x, y: d.y});
-    else if (e.target.id === 'p7') setPiece7Pos({x: d.x, y: d.y});
-    else console.log('drag error');
-
-    //snap to placement spots
+    const setPieceList = [setPiece1Pos, setPiece2Pos, setPiece3Pos, 
+      setPiece4Pos, setPiece5Pos, setPiece6Pos, setPiece7Pos];
+    const placementSpots = [{x:11, y:90}, {x:114, y:90}, {x:217, y:90},
+      {x:320, y:90}, {x:423, y:90}, {x:526, y:90}, {x:629, y:90}]
+    
+    for (let i=0; i<7; i++) {
+      if (e.target.id === `p${i}`) {
+        //snap to placement spots
+        if (d.y > 70 && d.y < 110) {
+          let foundPlacement = false;
+          for (let j=0; j<7; j++) {
+            if ( d.x > placementSpots[j].x - 30 && d.x < placementSpots[j].x + 30) {
+              setPieceList[i]({x: placementSpots[j].x, y: 90})
+              foundPlacement = true;
+            }
+          }
+          if (!foundPlacement) setPieceList[i]({x: d.x, y: d.y})
+        }
+        else setPieceList[i]({x: d.x, y: d.y})
+      }
+      else console.log('drag error');
+    }
   }
 
   return (
@@ -39,43 +51,43 @@ function Demo1() {
       </div>
       <Draggable position={piece1Pos} positionOffset={{x:0,y:-180}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p1' className='hex' style={{backgroundColor:'rgb(20,200,200)'}}>
+        <div id='p0' className='hex' style={{backgroundColor:'rgb(20,200,200)'}}>
           {`x: ${piece1Pos.x} y: ${piece1Pos.y}`}
         </div>
       </Draggable>
       <Draggable position={piece2Pos} positionOffset={{x:0,y:-260}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p2' className='hex' style={{backgroundColor:'rgb(200,20,200)'}}>
+        <div id='p1' className='hex' style={{backgroundColor:'rgb(200,20,200)'}}>
           {`x: ${piece2Pos.x} y: ${piece2Pos.y}`}
         </div>
       </Draggable>
       <Draggable position={piece3Pos} positionOffset={{x:0,y:-340}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p3' className='hex' style={{backgroundColor:'rgb(200,200,20)'}}>
+        <div id='p2' className='hex' style={{backgroundColor:'rgb(200,200,20)'}}>
           {`x: ${piece3Pos.x} y: ${piece3Pos.y}`}
         </div>
       </Draggable>
       <Draggable position={piece4Pos} positionOffset={{x:0,y:-420}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p4' className='hex' style={{backgroundColor:'rgb(20,100,100)'}}>
+        <div id='p3' className='hex' style={{backgroundColor:'rgb(20,100,100)'}}>
           {`x: ${piece4Pos.x} y: ${piece4Pos.y}`}
         </div>
       </Draggable>
       <Draggable position={piece5Pos} positionOffset={{x:0,y:-500}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p5' className='hex' style={{backgroundColor:'rgb(100,20,100)'}}>
+        <div id='p4' className='hex' style={{backgroundColor:'rgb(100,20,100)'}}>
           {`x: ${piece5Pos.x} y: ${piece5Pos.y}`}
         </div>
       </Draggable>
       <Draggable position={piece6Pos} positionOffset={{x:0,y:-580}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p6' className='hex' style={{backgroundColor:'rgb(100,100,20)'}}>
+        <div id='p5' className='hex' style={{backgroundColor:'rgb(100,100,20)'}}>
           {`x: ${piece6Pos.x} y: ${piece6Pos.y}`}
         </div>
       </Draggable>
       <Draggable position={piece7Pos} positionOffset={{x:0,y:-660}}
         bounds={{left:0,top:0,right:640,bottom:400}} onStop={onStop}>
-        <div id='p7' className='hex' style={{backgroundColor:'rgb(100,200,100)'}}>
+        <div id='p6' className='hex' style={{backgroundColor:'rgb(100,200,100)'}}>
           {`x: ${piece7Pos.x} y: ${piece7Pos.y}`}
         </div>
       </Draggable>
