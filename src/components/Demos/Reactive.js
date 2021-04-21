f8b9d79f20669a484585db04fee92113e313c8f1
jcxiao <chang.x1994@gmail.com>
2021-04-21 17:42:06 -0400

    Reworded responsive demo

--- src/components/Demos/Reactive.js
+++ src/components/Demos/Reactive.js
@@ -60,9 +60,9 @@ function ReactiveDemo () {
   return(
     <div className="demo-container" style={{ width:width }}>
       <div className="demo-exp">
-        <h4>Reactive layout</h4>
+        <h4>Responsive layout</h4>
         <p>
-          Reactive layout means that the placement of HTML elements on the screen changes
+          Responsive layout means that the placement of HTML elements on the screen changes
           based on the screensize of the device that is being used to view it. For example,
           the visible portion of the sidebar on this page and the navigation menu above will
           change, depending on whether you are viewing this page on a phone or a desktop PC.
